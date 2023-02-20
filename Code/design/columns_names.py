2e821e46a0a2f65a4089da6ecbaa8c5e50873a5f
permedtau <63590678+permedtau@users.noreply.github.com>
2023-02-20 14:36:18 +0200

    Add files

---
+++ Code/design/columns_names.py
@@ -0,0 +1,32 @@
+mental = {'screen_duration_total':'Screen-On Time [hour]',
+          'quest_mood':'Reported Mood Level',
+          'quest_stress':'Reported Stress Level',
+          'quest_meetings':'Reported Number of Encounters',
+         }
+
+sleep = {'awake_duration_in_seconds':'Awake Time [sec]',
+         'sleep_start_hour':'Sleep Start Hour',
+         'quest_sleep_time':'Reported Sleep Duration [hour]',
+         'quest_sleep_quality':'Reported Sleep Quality'         
+        }
+
+energy = {'steps':'Step Count',
+          'average_heart_rate_in_beats_per_minute':'Average Heart Rate [bpm]',
+          'still_ratio':'% Time Still',
+          'quest_sport_time':'Reported Sport Time [min]',
+          }
+
+
+columns_names = {'screen_duration_total':'Screen Time [hour]',
+                'quest_mood':'Reported Mood Level',
+                'quest_stress':'Reported Stress Level',
+                'quest_meetings':'Reported Number of Encounters',
+                'awake_duration_in_seconds':'Awake Time [sec]',
+                'sleep_start_hour':'Sleep Start Time [sec]',
+                'quest_sleep_time':'Reported Sleep Duration [hour]',
+                'quest_sleep_quality':'Reported Sleep Quality',
+                'still_ratio':'% Time Still',
+                'steps':'Step Count',
+                'average_heart_rate_in_beats_per_minute':'Average Heart Rate [bpm]',
+                'quest_sport_time':'Reported Sport Time [min]',
+                }
---
+++ Code/design/letters.py
@@ -0,0 +1,2 @@
+import string
+letters = [string.ascii_uppercase[i] for i in range(22)]
